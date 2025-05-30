77c6cf005c3e3b7832c0d487d9f4685aad9201cd
Victor May <mayvic@gmail.com>
2025-05-30 18:45:17 +0000

    Removing docker cache

--- gitchameleon/data_model.py
+++ gitchameleon/data_model.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, ConfigDict, model_validator
 from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, model_validator
+
 from gitchameleon.utils import load_jsonl
 
 
