b72ddcced6ac7ea8a2911310a723911c2124d5e4
Victor May <mayvic@gmail.com>
2025-06-09 01:19:50 +0000

    Adding persisted data mount

--- gitchameleon/eval_wrapper.py
+++ gitchameleon/eval_wrapper.py
@@ -48,7 +48,6 @@ def main():
     docker_command = [
         "docker",
         "run",
-        "--rm",
         "-it",
         # Mount the solution file (read-only)
         "-v",
@@ -59,6 +58,8 @@ def main():
         # Mount the target output file
         "-v",
         f"{output_csv_host_path}:/app/solution_eval_results.csv",
+         "-v",
+        f"persisted_data:/app/persisted_data",
         # Set the image and tag
         f"{args.docker_image}:{args.docker_tag}",
         # The command to execute inside the container
